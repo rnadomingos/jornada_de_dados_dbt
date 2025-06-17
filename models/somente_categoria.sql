e823b87da28f659c583b5a833b0fc6ad2972df6c
Renan Domingos <rna.domingos@hotmail.com>
2025-06-17 02:48:00 +0000

    fix: update table name  to google sheets worksheet

--- models/somente_categoria.sql
+++ models/somente_categoria.sql
@@ -3,7 +3,7 @@ WITH filtered as (
 	SELECT 
 		COALESCE(Categoria, 'Montadora') as categoria,
 		COUNT(*) as qtde
-	FROM {{ source('bronze', 'google_sheets_pesquisa')}} -- "google_sheets_Worksheet" gsw
+	FROM {{ source('bronze', 'google_sheets_Worksheet')}} -- "google_sheets_Worksheet" gsw
 	GROUP BY "Categoria" 
 	ORDER BY "Categoria" 
 )
